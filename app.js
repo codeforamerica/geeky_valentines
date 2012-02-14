6f6f0ece0d45454aadef1a583221e51b34339166
Mick Thompson <dthompson@gmail.com>
2012-02-13 22:02:29 -1000

    initial commit of valentine card project

---
+++ app.js
@@ -0,0 +1,142 @@
+//var request = require('request'),
+var express = require('express');
+var app = express.createServer(express.logger());
+var fs = require("fs");
+
+if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
+var available_cards = fs.readFileSync("cards.json", "utf-8");
+var card_data = JSON.parse(available_cards);
+
+var email = require('mailer');
+
+app.use(express.bodyParser());
+
+app.use('/static', express.static(__dirname + '/static')); 
+
+
+var randomString = function(length) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    var string_length = length || 8;
+    var randomstring = '';
+    for (var i=0; i<string_length; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum,rnum+1);
+    }
+    return randomstring;
+}
+
+
+app.get('/', function(req, res) {
+    res.render('index.ejs', { layout: false, card_data:card_data});
+});
+
+app.get('/email', function(req, res) {
+    res.render('email.ejs', { layout: false});
+});
+
+app.post('/send', function(req, res){
+
+    if(validateEmail(req.body.toaddress)){
+
+        var urlhash = randomString(8);
+        redis.set('postcard:'+urlhash, JSON.stringify({"to":{"name":req.body.toname,
+                                                             "address":req.body.toaddress},
+                                                       "from":{"name":req.body.fromname,
+                                                               "address":req.body.fromaddress},
+                                                       "message":req.body.message,
+                                                       "card":req.body.card,
+                                                       "date":(new Date()).getTime()}));
+
+        //Send email.
+        // include the from/to  and link to the message
+        var url = "http://hnlpostcard.herokuapp.com/postcard/"+urlhash
+        var email_template = fs.readFileSync("views/email.ejs", "utf-8");
+
+        email_template = email_template.replace("{{ toname }}", req.body.toname);
+        email_template = email_template.replace("{{ fromname }}", req.body.fromname);
+        var email_str = email_template.replace(/\{\{ url \}\}/g, url);
+
+        console.log("email", email_str);
+        email.send({
+            host: 'smtp.sendgrid.net',
+            port: '587',
+            authentication: 'plain',
+            username: process.env.SENDGRID_USERNAME,
+            password: process.env.SENDGRID_PASSWORD,
+            domain: 'heroku.com',
+            to: req.body.toaddress,
+            from: req.body.fromaddress,
+            subject: 'Aloha from Honolulu!',
+            html: email_str
+        }, function (err, result) {
+            console.log("sent email", err);
+        });
+        res.send({"status":"ok"});
+    }else{
+        res.send({"status":"fail"});
+    };
+});
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+app.get('/postcard/:id', function(req, res) {
+    redis.get("postcard:"+req.params.id, function(err, value) {
+        if(value == null){
+            res.redirect("/");
+            return;
+        }
+        var data = JSON.parse(value);
+        var date = new Date(data.date);
+        var card = card_data.cards[data.card];
+        card.name = data.card;
+        data.card = card;
+        data.datestring = months[date.getMonth()]+ " " + date.getDate()+", "+date.getFullYear();
+        res.render('postcard.ejs', { layout: false, data: data});
+    });
+
+    
+});
+
+
+app.get('/postcard1', function(req, res) {
+/*    redis.get("postcard:"+req.params.id, function(err, value) {
+        if(value == null){
+            res.redirect("/");
+            return;
+        }
+        var data = JSON.parse(value);
+        var date = new Date(data.date);
+        data.datestring = months[date.getMonth()]+ " " + date.getDate()+", "+date.getFullYear();
+*/
+    var card = card_data.cards["open_data"];
+    card.name = "open_data";
+    var data = {"to":{"name":"To Name",
+                      "address":"to@address"},
+                "from":{"name":"from name",
+                        "address":"from@address"},
+                "message":"test message",
+                "card":card,               
+                "date":(new Date()).getTime()};
+
+    res.render('postcard.ejs', { layout: false, data: data});
+
+    
+});
+
+
+function validateEmail(elementValue){  
+   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;  
+   return emailPattern.test(elementValue);  
+} 
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
---
+++ static/js/client.js
@@ -0,0 +1,60 @@
+var selected_card = "";
+$(document).ready(function(){
+    var howToTimer = 0;
+    var hideHowTo = function(){
+        $("div.howto").hide();
+        clearTimeout(howToTimer);
+        howToTimer = setTimeout(showHowTo, 5000);
+    }
+    var showHowTo = function(){
+        $("div.howto").fadeIn("normal");
+    };
+    hideHowTo();
+    $("div.aloha").click(function(){
+        if($(this).is(".flip")){
+            $("div.aloha").removeClass("flip");
+            hideHowTo();
+        }else{
+            $("div.aloha").addClass("flip");
+            hideHowTo();
+        }
+
+    });
+
+
+    $("input#submit").click(function(){
+        var postdata = {"toname":$("#toname").val(),
+                        "toaddress":$("#toaddress").val(),
+                        "fromname":$("#fromname").val(),
+                        "fromaddress":$("#fromaddress").val(),
+                        "message":$("#message").val(),
+                        "card":selected_card
+                       };
+        $.post("/send", postdata, function(data){
+            if(data.status == "ok"){
+                $("div.step").html("");
+                $("#form").html("<div class='status'>Card Sent!</div>");
+            }else{
+                $("div.steps").html("step oops: something went wrong, try again");
+            }
+        }, "json");
+    });
+    var card_valign = function(){
+        var top_margin = $(window).height() / 2 -250;
+        $(".card").css("margin-top", top_margin);
+        
+    };
+
+    $(window).resize(card_valign);
+    card_valign();
+
+
+    $("#cards li").click(function(){
+        selected_card = $(this).attr("id").split("-")[1];
+        $("#cards").fadeOut("250",function(){$("#form").fadeIn();});
+
+        $("div.step").html("step two: add a thoughful message");
+        
+    });
+
+});
